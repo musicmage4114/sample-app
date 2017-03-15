15495d79498f3e3098d7d3c0b91421eaadc49cb1
Zachary Lemmon <musicmage4114@aim.com>
2017-03-14 22:51:32 -0400

    Finish user edit, update, index, and destroy actions

--- Gemfile
+++ Gemfile
@@ -27,13 +27,16 @@ gem 'turbolinks', '5.0.1'
 gem 'jbuilder', '2.4.1'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 3.0'
-# Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
-
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
+
 gem 'bootstrap-sass', '3.3.6'
+# Use ActiveModel has_secure_password
 gem 'bcrypt', '3.1.11'
+
+gem 'faker', '1.6.6'
+gem 'will_paginate', '3.1.0'
+gem 'bootstrap-will_paginate', '0.0.10'
 
 group :development, :test do
   gem 'sqlite3', '1.3.12'
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,9 +1,11 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = users(:michael)
+  end
+
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
@@ -12,6 +14,13 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
     assert_select "a[href=?]", signup_path
+    log_in_as(@user)
+    get root_path
+    assert_select "a[href=?]", signup_path, count: 1
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", edit_user_path(@user)
+    assert_select "a[href=?]", logout_path
     get contact_path
     assert_select "title", full_title("Contact")
     get signup_path
