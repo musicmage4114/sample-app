d356542747231b9dfe18e06b8e653debc0c7887c
Zachary Lemmon <musicmage4114@aim.com>
2017-03-16 03:01:25 -0400

    Add user following

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.delivery_method = :smtp
-  host = 'vast-badlands-36625@herokuapp.com'
+  host = 'http://vast-badlands-36625.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address => 'smtp.sendgrid.net',
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,7 @@ require 'faker'
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+# Users
 User.create!(name: "Example User",
              email: "ejemplo@railstutorial.org",
              password: "foobar",
@@ -27,8 +28,17 @@ User.create!(name: "Example User",
                activated_at: Time.zone.now)
 end
 
+# Microposts
 users = User.order(:created_at).take(6)
 50.times do
   content = Faker::Lorem.sentence(5)
   users.each { |user| user.microposts.create!(content: content) }
 end
+
+# Following relationships
+users = User.all
+user = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
